5117e2a051f0c0adf44d7bc054ca2e4528c6191a
Hui Test <hui.he@massmail.state.ma.us>
2019-07-25 15:46:26 -0400

    second commit

---
+++ src/Test2.java
@@ -0,0 +1,13 @@
+
+public class Test2 {
+
+	String allocation ="";
+
+	public String getAllocation() {
+		return allocation;
+	}
+
+	public void setAllocation(String allocation) {
+		this.allocation = allocation;
+	}
+}
